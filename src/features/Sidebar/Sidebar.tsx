18f990fd19b7f4a60d61f956ba71d7c66dd54d12
pro_rokky <vlad6005@gmail.com>
2022-08-18 21:16:04 +0300

    feat(component): finish sidebar layout

--- src/features/Sidebar/Sidebar.tsx
+++ src/features/Sidebar/Sidebar.tsx
@@ -1,4 +1,7 @@
 import Budget from '@assets/Sidebar/budgetsIcon.svg'
+import Chart from '@assets/Sidebar/chartIcon.svg'
+import OperationsCategory from '@assets/Sidebar/operationsCategoryIcon.svg'
+import Operations from '@assets/Sidebar/operationsIcon.svg'
 import React from 'react'
 
 import styles from './styles/Sidebar.module.less'
@@ -12,10 +15,22 @@ export const Sidebar: React.FC = () => (
 			<p>Budgets</p>
 		</div>
 		<div className={ styles.pageLink }>
-			sec page
+			<div className={ styles.icon }>
+				<Operations />
+			</div>
+			<p>Operations</p>
 		</div>
 		<div className={ styles.pageLink }>
-			third page
+			<div className={ styles.icon }>
+				<OperationsCategory />
+			</div>
+			<p>Operations category</p>
+		</div>
+		<div className={ styles.pageLink }>
+			<div className={ styles.icon }>
+				<Chart />
+			</div>
+			<p>Chart of income and expenses</p>
 		</div>
 	</div>
 )
