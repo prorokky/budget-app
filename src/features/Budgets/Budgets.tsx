09ec0c9adc35c0c1e0cb514622d257186f687bc7
pro_rokky <vlad6005@gmail.com>
2022-09-11 22:30:12 +0300

    feat(component): add layout for budget card

--- src/features/Budgets/Budgets.tsx
+++ src/features/Budgets/Budgets.tsx
@@ -1,93 +1,16 @@
 import React from 'react'
 
 import { Header } from '../../components'
+import { BudgetCard } from './components'
 import styles from './styles/Budgets.module.less'
 
 export const Budgets: React.FC = () => (
 	<div className={ styles.container }>
 		<div className={ styles.listOfBudgets }>
 			<Header headerName='Budgets' />
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
-			<p>List of budgets</p>
+			<div className={ styles.budgetsContainer }>
+				<BudgetCard />
+			</div>
 		</div>
 		<div>
 			Budget info
